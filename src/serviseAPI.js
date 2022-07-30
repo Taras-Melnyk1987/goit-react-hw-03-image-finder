2119f67000ffce6e62d3e61df12730f3f51c36c2
Taras-Melnyk1987 <dr.melnuk1987@gmail.com>
2022-07-30 03:29:07 +0300

    ///

--- src/serviseAPI.js
+++ src/serviseAPI.js
@@ -4,19 +4,19 @@ const AUTH_TOKEN = '28905200-9345137f0018e9ed8026ecc0d';
 axios.defaults.baseURL = 'https://pixabay.com/api/';
 
 export default async function fetchImagesAPI(searchQuery, page) {
-    const options = new URLSearchParams({
-      key: `${AUTH_TOKEN}`,
-      q: `${searchQuery}`,
-      page: `${page}`,
-      image_type: 'photo',
-      orientation: 'horizontal',
-      per_page: '12',
-    });
-  
-    return axios.get(`?${options}`).then(response => {
-      if (response.status === 200) {
-        return response.data;
-      }
-      return Promise.reject(new Error('Ошибка, не загружается!'));
-    });
-  }
+  const options = new URLSearchParams({
+    key: `${AUTH_TOKEN}`,
+    q: `${searchQuery}`,
+    page: `${page}`,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    per_page: '12',
+  });
+
+  return axios.get(`?${options}`).then(response => {
+    if (response.status === 200) {
+      return response.data;
+    }
+    return Promise.reject(new Error('Ошибка, не загружается!'));
+  });
+}
